e2670bc8a1ef7353cc7eafd3f012ddc5d21df814
Grayson Cordell <Grayson.Cordell@vanderbilt.edu>
2024-08-21 11:50:34 -0500

    feat: +mistakes additions & stopOnFirstStep fix
    - Added ability for mistakeL type error declarations.
    - manualMistakesSearch for mistakes declared in regexPemdasMistakes
    - Mistake detection for add before multiply
    - fixed SimplifyCore misspelling of "stopOnFirstStep" and then fixed subsuquent errors caused by it actually stopping on first steps now.

--- lib/simplifyExpression/rules/commonRules.ts
+++ lib/simplifyExpression/rules/commonRules.ts
@@ -1,7 +1,5 @@
 /* eslint-disable antfu/consistent-list-newline */
-import {
-  ADD_FRACTIONS, COMMON_DENOMINATOR, DISTRIBUTE_NEGATIVE_ONE, DIVISION_BY_NEGATIVE_ONE, DIVISION_BY_ONE, KEMU_REMOVE_DOUBLE_FRACTION, MULTIPLY_BY_ZERO, MULTIPLY_FRACTIONS, PERCENTS_ADD, PERCENTS_CONVERT_TO_FRACTION, PERCENTS_SUB, REDUCE_EXPONENT_BY_ZERO, REDUCE_ZERO_NUMERATOR, REMOVE_ADDING_ZERO, REMOVE_EXPONENT_BASE_ONE, REMOVE_EXPONENT_BASE_ZERO, REMOVE_EXPONENT_BY_ONE, REMOVE_MULTIPLYING_BY_NEGATIVE_ONE, REMOVE_MULTIPLYING_BY_ONE, RESOLVE_DOUBLE_MINUS, SIMPLIFY_ARITHMETIC__ADD, SIMPLIFY_ARITHMETIC__MULTIPLY, SIMPLIFY_ARITHMETIC__POWER, SIMPLIFY_ARITHMETIC__SUBTRACT, SIMPLIFY_SIGNS,
-} from '~/types/ChangeTypes'
+import { ADD_FRACTIONS, COMMON_DENOMINATOR, DISTRIBUTE_NEGATIVE_ONE, DIVISION_BY_NEGATIVE_ONE, DIVISION_BY_ONE, KEMU_REMOVE_DOUBLE_FRACTION, MULTIPLY_BY_ZERO, MULTIPLY_FRACTIONS, PERCENTS_ADD, PERCENTS_CONVERT_TO_FRACTION, PERCENTS_SUB, REDUCE_EXPONENT_BY_ZERO, REDUCE_ZERO_NUMERATOR, REMOVE_ADDING_ZERO, REMOVE_EXPONENT_BASE_ONE, REMOVE_EXPONENT_BASE_ZERO, REMOVE_EXPONENT_BY_ONE, REMOVE_MULTIPLYING_BY_NEGATIVE_ONE, REMOVE_MULTIPLYING_BY_ONE, RESOLVE_DOUBLE_MINUS, SIMPLIFY_ARITHMETIC__ADD, SIMPLIFY_ARITHMETIC__MULTIPLY, SIMPLIFY_ARITHMETIC__POWER, SIMPLIFY_ARITHMETIC__SUBTRACT, SIMPLIFY_SIGNS } from '~/types/ChangeTypes'
 
 import Node from '../../node/index.js'
 import { math } from '~/config'
@@ -12,11 +10,11 @@ import { commonRuleMistakes } from '~/simplifyExpression/mistakes/commonRulesMis
 
 // Utilities for making nodes faster and more readable
 const makeConstant = (value: any) => Node.Creator.constant(value)
-const makeOperator = (operator: any, operands: any) => Node.Creator.operator(operator, operands)
+const makeOp = (operator: any, operands: any) => Node.Creator.operator(operator, operands)
 const makePercent = (value: any) => Node.Creator.percent(makeConstant(value))
 
 type AnyFunction = (...args: any[]) => any
-const createConstantOp = (operation: AnyFunction) => (node: any, vars: any) =>
+const makeConstantFromOp = (operation: AnyFunction) => (node: any, vars: any) =>
   makeConstant(operation(vars.c1.value, vars.c2.value))
 
 /**
@@ -48,9 +46,9 @@ const createConstantOp = (operation: AnyFunction) => (node: any, vars: any) =>
  *  A new optional 'mistake' property has been added to the rule object.
  *  This property is an array of rules that are mistake rules for the main rule.
  *  They will only be checked on matches of the main rule. This is to help find common mistakes while matching the main rule.
- *  Mistakes when specified as strings will find the rule from the rule pool. Ex. 'n1 + n2' -> { l: 'n1 + n2', id: 'SIMPLIFY_ARITHMETIC__ADD', replaceFct: createConstantOp(math.add) }
- *  Ids can be specified as well. Ex. 'SIMPLIFY_ARITHMETIC__ADD' -> { l: 'n1 + n2', id: 'SIMPLIFY_ARITHMETIC__ADD', replaceFct: createConstantOp(math.add) }
- *  [ 'n1 + n2', 'SIMPLIFY_ARITHMETIC__ADD' ] -> { l: 'n1 + n2', id: 'SIMPLIFY_ARITHMETIC__ADD', replaceFct: createConstantOp(math.add) }
+ *  Mistakes when specified as strings will find the rule from the rule pool. Ex. 'n1 + n2' -> { l: 'n1 + n2', id: 'SIMPLIFY_ARITHMETIC__ADD', replaceFct: makeConstantFromOp(math.add) }
+ *  Ids can be specified as well. Ex. 'SIMPLIFY_ARITHMETIC__ADD' -> { l: 'n1 + n2', id: 'SIMPLIFY_ARITHMETIC__ADD', replaceFct: makeConstantFromOp(math.add) }
+ *  [ 'n1 + n2', 'SIMPLIFY_ARITHMETIC__ADD' ] -> { l: 'n1 + n2', id: 'SIMPLIFY_ARITHMETIC__ADD', replaceFct: makeConstantFromOp(math.add) }
  *  If there is not a mistake rule that is also another rule, then you need to create a new one. l is not required for mistake rules.
  *  { id: 'ADDED_1_TOO_MANY', replaceFct: (node, vars) => makeConstant(math.add(vars.c1.value + 1, vars.c2.value)) }
  *
@@ -91,18 +89,18 @@ const _commonPoolOfRules = [
 
   // Constant arithmetic
   {
-    l: 'c1 + c2', id: SIMPLIFY_ARITHMETIC__ADD, replaceFct: createConstantOp(math.add),
+    l: 'c1 + c2', id: SIMPLIFY_ARITHMETIC__ADD, replaceFct: makeConstantFromOp(math.add),
   },
   {
-    l: 'c1 - c2', id: SIMPLIFY_ARITHMETIC__SUBTRACT, replaceFct: createConstantOp(math.subtract),
+    l: 'c1 - c2', id: SIMPLIFY_ARITHMETIC__SUBTRACT, replaceFct: makeConstantFromOp(math.subtract),
   },
   {
-    l: 'c1 * c2', id: SIMPLIFY_ARITHMETIC__MULTIPLY, replaceFct: createConstantOp(math.multiply),
+    l: 'c1 * c2', id: SIMPLIFY_ARITHMETIC__MULTIPLY, replaceFct: makeConstantFromOp(math.multiply),
   },
   {
     l: 'c1 ^ c2',
     id: SIMPLIFY_ARITHMETIC__POWER,
-    replaceFct: createConstantOp(math.pow),
+    replaceFct: makeConstantFromOp(math.pow),
   },
 
   // Percents
@@ -147,18 +145,45 @@ const _commonPoolOfRules = [
 
       const newNum1 = math.equal(denom1.value, lcm.value)
         ? num1
-        : makeOperator('*', [num1, makeConstant(math.divide(lcm.value, denom1.value))])
+        : makeOp('*', [num1, makeConstant(math.divide(lcm.value, denom1.value))])
 
       const newNum2 = math.equal(denom2.value, lcm.value)
         ? num2
-        : makeOperator('*', [num2, makeConstant(math.divide(lcm.value, denom2.value))])
+        : makeOp('*', [num2, makeConstant(math.divide(lcm.value, denom2.value))])
 
-      return makeOperator('/', [makeOperator('+', [newNum1, newNum2]), lcm])
+      return makeOp('/', [makeOp('+', [newNum1, newNum2]), lcm])
     },
   },
+
+  // Pemdas mistakes - This does not work correctly in all cases right now. It is disabled. But this is the current format for making mistakes with custom L values
+  // { // Added instead of multiplied
+  //  mistakeL: 'c1 + c2 * c3',
+  //  mistakes: [{
+  //    id: 'PEMDAS__ADD_INSTEAD_OF_MULTIPLY',
+  //    replaceFct: (node: any, vars: any) => {
+  //      const added = math.add(vars.c1.value, vars.c2.value)
+  //      const multiplied = makeOp('*', [makeConstant(added), vars.c3])
+  //      return multiplied
+  //    },
+  //  }],
+  // },
 ]
 // Add mistakes found in commonRulesMistakes.ts to the rules
+// Also if mistakeL is specified, then add mistakeL to the mistakes
 _commonPoolOfRules.forEach((rule: any) => {
+  // If mistakeL is specified, then add mistakeL to the mistakes
+  if (rule.mistakeL) {
+    rule.mistakes = rule.mistakes.map((mistake: any) => {
+      mistake.l = rule.mistakeL
+      if (rule.id && !mistake.id)
+        mistake.id = rule.id
+      return mistake
+    })
+    if (!rule.id)
+      rule.id = rule.mistakes[0].id
+  }
+
+  // Add CommonRuleMistakes to the rules using their placeIn property
   const mistakesForRule: any[] = commonRuleMistakes.filter((mistake: any) => {
     const idAndL = cleanString(mistake.placeIn).split('|') // id | l1 .split('|')
     return cleanString(rule.id) === idAndL[0] && cleanString(rule.l) === idAndL[1]
