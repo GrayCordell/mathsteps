9099e3c9e9db20662a78116ff3a0207610db24ab
Grayson Cordell <Grayson.Cordell@vanderbilt.edu>
2024-08-23 11:09:31 -0500

    feat: sorted ChangeTypes&ErrorTypes

--- lib/types/ErrorTypes.ts
+++ lib/types/ErrorTypes.ts
@@ -1,5 +1,29 @@
 import ChangeTypes from '~/types/ChangeTypes'
 
+export const ADDED_ONE_TOO_FEW = 'ADDED_ONE_TOO_FEW' as const
+export const ADDED_ONE_TOO_MANY = 'ADDED_ONE_TOO_MANY' as const
+export const ADDED_INSTEAD_OF_MULTIPLIED = 'ADDED_INSTEAD_OF_MULTIPLIED' as const
+export const MULTIPLIED_INSTEAD_OF_ADDED = 'MULTIPLIED_INSTEAD_OF_ADDED' as const
+export const SUBTRACTED_INSTEAD_OF_ADDED = 'SUBTRACTED_INSTEAD_OF_ADDED' as const
+export const MULTIPLIED_ONE_TOO_MANY = 'MULTIPLIED_ONE_TOO_MANY' as const
+export const MULTIPLIED_ONE_TOO_FEW = 'MULTIPLIED_ONE_TOO_FEW' as const
+
+export const MultiplicationErrors = {
+  ADDED_INSTEAD_OF_MULTIPLIED,
+  MULTIPLIED_INSTEAD_OF_ADDED,
+  SUBTRACTED_INSTEAD_OF_ADDED,
+  MULTIPLIED_ONE_TOO_MANY,
+  MULTIPLIED_ONE_TOO_FEW,
+}
+
+export const AdditionErrors = {
+  ADDED_ONE_TOO_FEW,
+  ADDED_ONE_TOO_MANY,
+  SUBTRACTED_INSTEAD_OF_ADDED,
+}
+
 export default {
   ...ChangeTypes,
+  ...MultiplicationErrors,
+  ...AdditionErrors,
 } as const
