fab038216b4f6fcdcd10f2ad22d48ec80675cc96
Grayson Cordell <Grayson.Cordell@vanderbilt.edu>
2024-08-23 15:38:01 -0500

    chore: typed EqualityCache

--- lib/util/equalityCache.ts
+++ lib/util/equalityCache.ts
@@ -1,29 +1,32 @@
 export class DisjointSet {
+  private parent: Map<string, string>
+  private rank: Map<string, number>
+
   constructor() {
-    this.parent = new Map()
-    this.rank = new Map() // To store the rank (or size) of each set
+    this.parent = new Map<string, string>()
+    this.rank = new Map<string, number>() // To store the rank (or size) of each set
   }
 
   // Find the representative of the set containing x with path compression
-  find(x) {
+  find(x: string): string {
     if (!this.parent.has(x)) {
       this.parent.set(x, x)
       this.rank.set(x, 0) // Initialize rank
     }
     if (this.parent.get(x) !== x) {
-      this.parent.set(x, this.find(this.parent.get(x)))
+      this.parent.set(x, this.find(this.parent.get(x)!))
     }
-    return this.parent.get(x)
+    return this.parent.get(x)!
   }
 
   // Union the sets containing x and y by rank
-  union(x, y) {
+  union(x: string, y: string): void {
     const rootX = this.find(x)
     const rootY = this.find(y)
 
     if (rootX !== rootY) {
-      const rankX = this.rank.get(rootX)
-      const rankY = this.rank.get(rootY)
+      const rankX = this.rank.get(rootX)!
+      const rankY = this.rank.get(rootY)!
 
       if (rankX > rankY) {
         this.parent.set(rootY, rootX)
@@ -40,16 +43,19 @@ export class DisjointSet {
 }
 
 export class EqualityCache {
-  constructor(customEqualityFn = null, cache = new DisjointSet()) {
+  private customEqualityFn: ((str1: string, str2: string) => boolean) | null
+  private cache: DisjointSet
+
+  constructor(customEqualityFn: ((str1: string, str2: string) => boolean) | null = null, cache: DisjointSet = new DisjointSet()) {
     this.customEqualityFn = customEqualityFn
     this.cache = cache
   }
 
-  getFromCachedEquality(str1) {
+  getFromCachedEquality(str1: string): string {
     return this.cache.find(str1)
   }
 
-  getCachedEquality(str1, str2, customEqualityFn = this.customEqualityFn) {
+  getCachedEquality(str1: string, str2: string, customEqualityFn: ((str1: string, str2: string) => boolean) | null = this.customEqualityFn): boolean {
     const root1 = this.cache.find(str1)
     const root2 = this.cache.find(str2)
 
@@ -57,7 +63,7 @@ export class EqualityCache {
       return true // They are in the same set, so they are equal
 
     // If not, compute the result using the custom function
-    const result = customEqualityFn(str1, str2)
+    const result = customEqualityFn ? customEqualityFn(str1, str2) : false
 
     if (result) {
       // If they are equal, union their sets
