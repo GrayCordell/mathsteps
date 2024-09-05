86c3462141f5bf2c6544ff8e29d82f30f0e5a95e
Grayson Cordell <Grayson.Cordell@vanderbilt.edu>
2024-09-05 10:25:44 -0500

    feat:  add currently unused subtraction error types

--- lib/types/ErrorTypes.ts
+++ lib/types/ErrorTypes.ts
@@ -6,13 +6,20 @@ export const ADDED_ONE_TOO_MANY = 'ADDED_ONE_TOO_MANY' as const
 export const ADDED_INSTEAD_OF_MULTIPLIED = 'ADDED_INSTEAD_OF_MULTIPLIED' as const
 export const MULTIPLIED_INSTEAD_OF_ADDED = 'MULTIPLIED_INSTEAD_OF_ADDED' as const
 export const SUBTRACTED_INSTEAD_OF_ADDED = 'SUBTRACTED_INSTEAD_OF_ADDED' as const
+
+export const ADDED_INSTEAD_OF_SUBTRACTED = 'ADDED_INSTEAD_OF_SUBTRACTED' as const
+export const MULTIPLIED_INSTEAD_OF_SUBTRACTED = 'MULTIPLIED_INSTEAD_OF_SUBTRACTED' as const
+export const SUBTRACTED_INSTEAD_OF_MULTIPLIED = 'SUBTRACTED_INSTEAD_OF_MULTIPLIED' as const
+export const SUBTRACTED_ONE_TOO_MANY = 'SUBTRACTED_ONE_TOO_MANY' as const
+export const SUBTRACTED_ONE_TOO_FEW = 'SUBTRACTED_ONE_TOO_FEW' as const
+
 export const MULTIPLIED_ONE_TOO_MANY = 'MULTIPLIED_ONE_TOO_MANY' as const
 export const MULTIPLIED_ONE_TOO_FEW = 'MULTIPLIED_ONE_TOO_FEW' as const
 
 export const MultiplicationErrors = {
   ADDED_INSTEAD_OF_MULTIPLIED,
+  SUBTRACTED_INSTEAD_OF_MULTIPLIED,
   MULTIPLIED_INSTEAD_OF_ADDED,
-  SUBTRACTED_INSTEAD_OF_ADDED,
   MULTIPLIED_ONE_TOO_MANY,
   MULTIPLIED_ONE_TOO_FEW,
 } as const
@@ -22,6 +29,12 @@ export const AdditionErrors = {
   ADDED_ONE_TOO_MANY,
   SUBTRACTED_INSTEAD_OF_ADDED,
 } as const
+export const isAdditionError = (errorType: AMistakeType) => Object.values(AdditionErrors).includes(errorType as any)
+export const SubtractionErrors = {
+  SUBTRACTED_ONE_TOO_FEW,
+  SUBTRACTED_ONE_TOO_MANY,
+  ADDED_INSTEAD_OF_SUBTRACTED,
+} as const
 
 export const UNKNOWN = 'UNKNOWN' as const
 export const NO_CHANGE = 'NO_CHANGE' as const
@@ -29,8 +42,28 @@ export const MistakeTypes = {
   ...ChangeTypes, // Also include the ChangeTypes
   ...MultiplicationErrors,
   ...AdditionErrors,
+  ...SubtractionErrors,
   UNKNOWN,
   NO_CHANGE,
 } as const
 export type AMistakeType = typeof MistakeTypes[keyof typeof MistakeTypes] | AChangeType | null
 export default MistakeTypes
+
+export const convertAdditionToSubtractionErrorType = (errorType: AMistakeType) => {
+  switch (errorType) {
+    case ADDED_ONE_TOO_FEW: return SUBTRACTED_ONE_TOO_FEW
+    case ADDED_ONE_TOO_MANY: return SUBTRACTED_ONE_TOO_MANY
+    case ADDED_INSTEAD_OF_MULTIPLIED: return SUBTRACTED_INSTEAD_OF_MULTIPLIED
+    case MULTIPLIED_INSTEAD_OF_ADDED: return MULTIPLIED_INSTEAD_OF_SUBTRACTED
+    case SUBTRACTED_INSTEAD_OF_ADDED: return ADDED_INSTEAD_OF_SUBTRACTED
+    case null: return null
+    default: {
+      const newErrorType = errorType.replace('SUBTRACTED', 's0').replace('SUBTRACT', 's1').replace('ADDED', 'a0').replace('ADD', 'a1')
+        .replace('s0', 'ADDED')
+        .replace('s1', 'ADD')
+        .replace('a0', 'SUBTRACTED')
+        .replace('a1', 'SUBTRACT') as AMistakeType
+      return newErrorType
+    }
+  }
+}
