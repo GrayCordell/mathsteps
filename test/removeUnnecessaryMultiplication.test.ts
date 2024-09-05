6393aac038e7e01930e14175c8736e9959563fe2
Grayson Cordell <Grayson.Cordell@vanderbilt.edu>
2024-09-05 10:26:11 -0500

    Create removeUnnecessaryMultiplication.test.ts

---
+++ test/removeUnnecessaryMultiplication.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from 'vitest'
+import type { MathNode } from 'mathjs'
+import { removeUnnecessaryMultiplications } from '~/newServices/nodeServices/removeUnnecessaryMultiplications'
+import { parseText } from '~/index'
+import { myNodeToString } from '~/newServices/nodeServices/myNodeToString'
+import { cleanString } from '~/util/stringUtils'
+
+describe('removeUnnecessaryMultiplications', () => {
+  const testCases: { input: string, expected: string }[] = [
+    { input: '8+1*2/4*4', expected: '8+2/4*4' },
+    { input: '1 * 2', expected: '2' },
+    { input: '2 * 1', expected: '2' },
+    { input: '(1 * 2) / (2 * 1)', expected: '2 / 2' },
+    { input: '3 * 1 * 4', expected: '3 * 4' },
+    { input: '1 * 1 * 5', expected: '5' },
+    { input: '1', expected: '1' },
+    { input: 'x * 1', expected: 'x' },
+    { input: '1 * x', expected: 'x' },
+    { input: 'x * y * 1', expected: 'x * y' },
+    { input: '1 * (x + 1)', expected: 'x + 1' },
+    { input: '(x + 1) * 1', expected: 'x + 1' },
+  ]
+
+  testCases.forEach(({ input, expected }) => {
+    it(`removes unnecessary *1 from "${input}" and simplifies to "${expected}"`, () => {
+      expected = cleanString(expected)
+      input = cleanString(input)
+      const expression: MathNode = parseText(input)
+
+      const simplifiedExpression: MathNode = removeUnnecessaryMultiplications(expression)
+
+      expect(cleanString(myNodeToString(simplifiedExpression))).toBe(cleanString(expected))
+    })
+  })
+})
