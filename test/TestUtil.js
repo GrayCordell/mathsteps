2eb2655ceb6804e502cdfec9e81c9e161945c44d
Grayson Cordell <Grayson.Cordell@vanderbilt.edu>
2024-09-05 10:26:51 -0500

    refactor:TestUtil flatten->kemuFlatten

--- test/TestUtil.js
+++ test/TestUtil.js
@@ -3,12 +3,11 @@ import print from '../lib/util/print.js'
 import { parseText } from '~/index'
 import { kemuFlatten } from '~/simplifyExpression/kemuSimplifyCommonServices.js'
 
-const flatten = kemuFlatten
 // TestUtil contains helper methods to share code across tests
 const TestUtil = {}
 // Takes in an input string and returns a flattened and parsed node
 TestUtil.parseAndFlatten = function (exprString) {
-  return flatten(parseText(exprString))
+  return kemuFlatten(parseText(exprString))
 }
 // Tests a function that takes an input string and check its output
 TestUtil.testFunctionOutput = function (fn, input, output) {
@@ -19,14 +18,14 @@ TestUtil.testFunctionOutput = function (fn, input, output) {
 // tests a function that takes in a node and returns a boolean value
 TestUtil.testBooleanFunction = function (simplifier, exprString, expectedBooleanValue) {
   it(`${exprString} ${expectedBooleanValue}`, () => {
-    const inputNode = flatten(parseText(exprString))
+    const inputNode = kemuFlatten(parseText(exprString))
     assert.equal(simplifier(inputNode), expectedBooleanValue)
   })
 }
 // Tests a simplification function
 TestUtil.testSimplification = function (simplifyingFunction, exprString, expectedOutputString) {
   it(`${exprString} -> ${expectedOutputString}`, () => {
-    assert.deepEqual(print.ascii(simplifyingFunction(flatten(parseText(exprString))).rootNode), expectedOutputString)
+    assert.deepEqual(print.ascii(simplifyingFunction(kemuFlatten(parseText(exprString))).rootNode), expectedOutputString)
   })
 }
 export default TestUtil
