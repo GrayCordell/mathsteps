6e2306915c9de9893b67cbf51bf10edf81d149a0
Grayson Cordell <Grayson.Cordell@vanderibilt.edu>
2024-09-10 18:35:36 -0500

    bugfix: fix more +- equality

--- lib/newServices/expressionEqualsAndNormalization.ts
+++ lib/newServices/expressionEqualsAndNormalization.ts
@@ -6,10 +6,11 @@ import { kemuFlatten, kemuNormalizeConstantNodes } from '~/simplifyExpression/ke
 import { removeImplicitMultiplicationFromNode } from '~/newServices/treeUtil'
 import type { MathNode } from 'mathjs'
 import type { EqualityCache } from '~/util/equalityCache'
+import { makePlusMinusMinusAndReturnString } from '~/newServices/nodeServices/removeUnnecessaryMultiplications'
 
 export function veryNormalizeNode(node_: MathNode | string): MathNode {
   let node = (typeof node_ === 'string') ? parseText(node_) : node_
-  node = kemuSortArgs(kemuNormalizeConstantNodes(removeImplicitMultiplicationFromNode(kemuFlatten(node))), true)
+  node = kemuSortArgs(kemuNormalizeConstantNodes(removeImplicitMultiplicationFromNode(kemuFlatten(node))), true) // TODO Its possible there could be issues if we don't remove +- here too. (We can't right now because it requires a parse)
   return <MathNode>node
 }
 
@@ -19,11 +20,13 @@ export function areExpressionEqual(exp0: string | MathNode, exp1: string | MathN
   if (exp0 === exp1) // same object reference. Or also same exact string but we will check "cleaned" strings later.
     return true
 
-  const strExp0 = typeof exp0 === 'string' ? exp0 : myNodeToString(exp0)
-  const strExp1 = typeof exp1 === 'string' ? exp1 : myNodeToString(exp1)
-  if (cleanString(strExp0) === cleanString(strExp1)) // 'cleaned' string check
-    return true
+  let strExp0 = typeof exp0 === 'string' ? cleanString(exp0) : myNodeToString(exp0)
+  let strExp1 = typeof exp1 === 'string' ? cleanString(exp1) : myNodeToString(exp1)
+  strExp0 = makePlusMinusMinusAndReturnString(strExp0)
+  strExp1 = makePlusMinusMinusAndReturnString(strExp1)
 
+  if (strExp0 === strExp1) // 'cleaned' string check
+    return true
   if (!equalityCache)
     return areExpressionEqualCore(strExp0, strExp1)
   else
