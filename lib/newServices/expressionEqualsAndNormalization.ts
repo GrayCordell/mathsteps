cb43d58a2b886e5e89dd5ed1da1db8556f9b0e75
Grayson Cordell <Grayson.Cordell@vanderibilt.edu>
2024-09-20 17:37:01 -0500

    feat/bugfix: decimals === fractions for node equality

--- lib/newServices/expressionEqualsAndNormalization.ts
+++ lib/newServices/expressionEqualsAndNormalization.ts
@@ -1,4 +1,5 @@
 import type { MathNode } from 'mathjs'
+import { ConstantNode, isConstantNode, isOperatorNode } from '~/config'
 import { parseText } from '~/index'
 import { myNodeToString } from '~/newServices/nodeServices/myNodeToString.js'
 import { makePlusMinusMinusAndReturnString } from '~/newServices/nodeServices/removeUnnecessaryMultiplications'
@@ -10,7 +11,7 @@ import kemuSortArgs from '../simplifyExpression/kemuSortArgs.js'
 
 export function veryNormalizeNode(node_: MathNode | string): MathNode {
   let node = (typeof node_ === 'string') ? parseText(node_) : node_
-  node = kemuSortArgs(kemuNormalizeConstantNodes(removeImplicitMultiplicationFromNode(kemuFlatten(node))), true) // TODO Its possible there could be issues if we don't remove +- here too. (We can't right now because it requires a parse)
+  node = kemuSortArgs(kemuNormalizeConstantNodes(convertAllSimpleFractionsToDecimals(removeImplicitMultiplicationFromNode(kemuFlatten(node)))), true) // TODO Its possible there could be issues if we don't remove +- here too. (We can't right now because it requires a parse)
   return node
 }
 
@@ -39,14 +40,40 @@ function areExpressionEqualCore(exp0?: string | MathNode | null, exp1?: string |
   if (exp0 === exp1) // same object reference. Or also same exact string but we will check "cleaned" strings later.
     return true
 
-  const strExp0 = typeof exp0 === 'string' ? exp0 : myNodeToString(exp0)
-  const strExp1 = typeof exp1 === 'string' ? exp1 : myNodeToString(exp1)
-
-  if (cleanString(strExp0) === cleanString(strExp1)) // 'cleaned' string check
+  const strExp0 = typeof exp0 === 'string' ? cleanString(exp0) : myNodeToString(exp0)
+  const strExp1 = typeof exp1 === 'string' ? cleanString(exp1) : myNodeToString(exp1)
+  if (strExp0 === strExp1) // 'cleaned' string check
     return true
 
   const newExpNode0 = veryNormalizeNode(strExp0)
   const newExpNode1 = veryNormalizeNode(strExp1)
 
   return newExpNode0.equals(newExpNode1) // node equal check.
+}
+
+
+function convertAllSimpleFractionsToDecimals(node: MathNode): MathNode {
+  // Function to traverse the expression and convert fractions to decimals
+  const convertFractions = (node: MathNode) => {
+    // If the node is a binary operator (like '/') and the operator is division
+    if (isOperatorNode(node) && node.op === '/' && node.args.length === 2 && isConstantNode(node.args[0]) && isConstantNode(node.args[1])) {
+      // Evaluate the left and right sides of the division
+      const leftValue = node.args[0].value
+      const rightValue = node.args[1].value
+
+      // Return the decimal result of the division
+      return new ConstantNode(leftValue / rightValue)
+    }
+
+    // Recursively apply the transformation for all child nodes
+    if (isOperatorNode(node) && node.args) {
+      node.args = node.args.map(convertFractions)
+    }
+
+    return node
+  }
+
+  // Apply the transformation to the whole expression
+  // Return the modified expression as a node
+  return convertFractions(node)
 }
