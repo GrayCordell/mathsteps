f09270a12e34979f69df24b32d07ff3e30e0b842
Grayson Cordell <Grayson.Cordell@vanderibilt.edu>
2025-01-09 18:39:15 -0600

    chore: cleanup evaluateSteps2

--- test/evaluateSteps2.test.ts
+++ test/evaluateSteps2.test.ts
@@ -1,31 +1,10 @@
 import { describe, expect, it } from 'vitest'
 import mathsteps, { myNodeToString } from '~/index'
-import { solveEquation } from '~/indexPrepareSimplifyAndSolve'
 
 import { assessUserSteps } from '~/simplifyExpression/stepEvaluationCore'
 import { RANDOM_EXPRESSIONS } from '~/util/randomExpression'
 import { cleanString } from '~/util/stringUtils'
 
-export function getNodeStepsToSolveEquation(equation: string): {
-  equation: {
-    left: { type: string, node: any }
-    right: { type: string, node: any }
-  }
-  equationString: string
-}[] {
-  const equationSteps: any[] = []
-  const unknownVariable = equation.toLowerCase().match(/[a-z]/i)?.[0] ?? 'x'
-  /* const eventualAnswer = */ solveEquation({
-    equationAsText: equation,
-    unknownVariable,
-    onStepCb(step: any) {
-      equationSteps.push({ ...step, equationString: step.equation.toString() })
-      // console.log(`[ ${step.equation.getId()} ] ${step.stepId} | ${step.equation}`)
-    },
-  })
-  // console.log('equationSteps', equationSteps)
-  return equationSteps
-}
 export function getNodeStepsToSolveExpression(userStep: string) {
   userStep = cleanString(userStep)
   const steps: any[] = []
