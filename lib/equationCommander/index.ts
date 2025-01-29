b632ea71ec4e12553c41b34bb060d201025e3990
Grayson Cordell <Grayson.Cordell@vanderbilt.edu>
2025-01-29 11:15:28 -0600

    bufix: getFastestAmountOfStepsToSolveEquation
    - Corrected for substeps and removing combined steps

--- lib/equationCommander/index.ts
+++ lib/equationCommander/index.ts
@@ -5,10 +5,42 @@ import { findMatchingAddRemoveTerms, findNonAddRemoveRuleMatches, makeEquationPr
 import { getAssessSolvedProblemSteps } from '~/kemuEquation/SimpleSolveEquationFunction'
 import { areEquationsEqual } from '~/newServices/expressionEqualsAndNormalization'
 import type { ProcessedEquation } from '~/simplifyExpression/equationEvaluation'
+import type { StepInfo } from '~/simplifyExpression/stepEvaluationCore'
+import { isAddTermChangeType, isRemoveTermChangeType } from '~/types/changeType/changeAndMistakeUtils'
 import { ChangeTypes } from '~/types/changeType/ChangeTypes'
 import type { AMathRule } from '~/types/changeType/MathRuleTypes'
 import { getChangesTypesForRule } from '~/types/changeType/MathRuleTypes'
 import { UndoableString } from '~/util/UndoableString'
+
+export const getFastestAmountOfStepsToSolveEquation = (assessedSteps: ProcessedEquation[]): number => {
+  // 1. Filter out all NO_CHANGE steps
+  // 2. Remove all removeTerms and addterms from right. (We will use all from left)
+  // 3. remove all EQ_SWAP_SIDES on the right. (We only need 1)
+  // 4. remove all EQ_MULTIPLY_BOTH_SIDES_BY_NEGATIVE_ONE on the right. (We only need 1)
+
+  const removeNoChangeTypeStepsFn = (side: StepInfo[]) => side.filter(s => s.attemptedChangeType !== ChangeTypes.NO_CHANGE)
+  const removeStuffFromRightFn = (side: StepInfo[]) => {
+    return side
+      .filter(s => !isRemoveTermChangeType(s.attemptedChangeType)) // 2.
+      .filter(s => !isAddTermChangeType(s.attemptedChangeType)) // 2.
+      .filter(s => s.attemptedChangeType !== ChangeTypes.EQ_SWAP_SIDES) // 3.
+      .filter(s => s.attemptedChangeType !== ChangeTypes.EQ_MULTIPLY_BOTH_SIDES_BY_NEGATIVE_ONE) // 4.
+  }
+
+  assessedSteps = assessedSteps.map((step) => {
+    const left = removeNoChangeTypeStepsFn(step.left)
+    const right = removeNoChangeTypeStepsFn(removeStuffFromRightFn(step.right))
+    return { ...step, left, right }
+  })
+
+  // Now we need to count all the sub-steps
+  let count = 0
+  for (const step of assessedSteps) {
+    count += step.left.length
+    count += step.right.length
+  }
+  return count
+}
 
 // TODO remove redudant code. Make more efficient, repeated calls of assessedSteps and find all options.
 export class EquationCommander {
@@ -41,9 +73,8 @@ export class EquationCommander {
 
   getAssessedSteps = (): ProcessedEquation[] => this.assessedSteps
   getCorrectAnswerSteps = (): { equationString: string }[] => this.correctAnswerSteps
-  getFastestAmountOfStepsToSolve = (): number => this.assessedSteps.length
+  getFastestAmountOfStepsToSolve = (): number => getFastestAmountOfStepsToSolveEquation(this.assessedSteps)
   getCurrentNumberOfSteps = (): number => this.assessedSteps.length
-
   getCurrentMatches(): EqLRStepWithNewTo[] {
     const currentEquation = this.getValue()
     // Build up the possible matches
